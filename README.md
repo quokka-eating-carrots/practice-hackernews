8ed5e9959e4e49e0a843253fe7470c37907860fd
quokka-eating-carrots <ovzip7@gmail.com>
2022-10-14 14:56:33 +0900

    feat: Create short code, Create router
    DOM API를 사용하지 않고 코드들을 더 짧게 정리하였고, 라우터라는 개념을
    넣어서 페이지 간 이동할 수 있게 함

--- README.md
+++ README.md
@@ -37,3 +37,83 @@ for(let i = 0; i < 10; i += 1) {
   ajax.send();
 ```
 강사님은 `slice()` 대신에 `substr()`이라는 메소드를 썼는데 그건 이제 사용되지 않는다고 하여서 `slice()`를 사용했다.
+
+## 221014
+
+어제 작성했던 for문 안에 HTML 구조들을 `.innerHTML` 속성을 사용해서 조금 더 직관적인 코드로 만들어 주었습니다.
+```javascript
+for(let i = 0; i < 10; i += 1) {
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+  <li>
+    <a href="#${newsFeed[i].id}">
+    ${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+  </li>`;
+
+  ul.appendChild(div.firstElementChild);
+}
+```
+여기서 보여지는 `div` 변수는 임시적인 `DOM`이라고 설명을 주셨는데... 사실 이해가 잘 안 돼서... ㅠㅠ 왜냐면 결과값의 HTML을 보면 `div` 태그는 없기 때문이다. 지금은 간단하게 div의 첫 번째 자식 요소를 ul 바꿔 준다는 정도로 이해하면 될 것 같다. ~~이게아닐가능성이굉장히높을듯싶지만~~
+
+++ 팀원들의 도움으로 이해한 것! `div`의 첫 번째 자식 요소를 `ul`태그 안으로 넣는 것으로 이해하면 됨!
+
+```javascript
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
+
+  return JSON.parse(ajax.response);
+};
+```
+JS를 공부하면서 꾸준히 보는 게 반복되는 코드를 간단한 변수나 함수로 설정해서 코드의 길이가 너무 길어지지 않게 하는 것이었다. 여기에서는 API 서버를 받아서, 다시 쓸 곳으로 보내는 게 반복이 되는데 변경되어야 하는 `url`을 인수로 받아서 사용하면 반복되는 곳에서 인수만 변경해서 사용하면 된다.
+
+```javascript
+const newsFeed = getData(NEWS_URL);
+// newsfeed를 받을 때
+
+const newsContent = getData(CONTENT_URL.replace('@id', id))
+// newscontent를 받을 때
+```
+
+```javascript
+const newsList = [];
+
+newsList.push('<ul>');
+
+for(let i = 0; i < 10; i += 1) {
+  newsList.push(`
+  <li>
+    <a href="#${newsFeed[i].id}">
+    ${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+  </li>
+  `);
+}
+
+newsList.push('</ul>');
+
+container.innerHTML = newsList.join('');
+```
+
+`newsList`라는 빈 배열을 만들어 주고, 거기에 `ul` 여는 태그와 닫는 태그를 for문이 시작되기 전, 후로 넣어 줍니다. 그리고 마지막에 `container (id = "root"인 div 태그입니다)`에 `join` 속성에 공백을 사용하여 `push`한 내용들이 `,`로 배열이 되지 않게 만들어 줍니다.
+
+**라우터**
+라우터의 간단한 설명을 하자면 상위 통신망과 하위 통신망 사이를 중계해 주는 기계라고 한다. 여기서 라우터라고 하면 상위 사이트와 하위 사이트, 즉 메인 페이지와 콘텐츠(하위) 페이지를 이어 주는 용도 정도가 될 것 같다.
+
+```javascript
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  } 
+}
+
+window.addEventListener('hashchange', router);
+
+router();
+```
+
+`addEventListener` 부분에 router을 넣어서 hash가 빈값이면 목록을 그게 아니라면 콘텐츠를 보여 주게 만들어 주었다. `router`는 함수이기 때문에 마지막에 호출까지 해 주었다.
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -123,29 +123,37 @@ var ajax = new XMLHttpRequest();
 var content = document.createElement('div');
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-var newsFeed = JSON.parse(ajax.response);
-var ul = document.createElement('ul');
-window.addEventListener('hashchange', function () {
-  var id = location.hash.slice(1);
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+function getData(url) {
+  ajax.open('GET', url, false);
   ajax.send();
-  var newsContent = JSON.parse(ajax.response);
-  var title = document.createElement('h1');
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-});
-for (var i = 0; i < 10; i += 1) {
-  var li = document.createElement('li');
-  var a = document.createElement('a');
-  a.href = "#".concat(newsFeed[i].id);
-  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
-  li.appendChild(a);
-  ul.appendChild(li);
+  return JSON.parse(ajax.response);
 }
-container.appendChild(content);
-container.appendChild(ul);
+;
+function newsFeed() {
+  var newsFeed = getData(NEWS_URL);
+  var newsList = [];
+  newsList.push('<ul>');
+  for (var i = 0; i < 10; i += 1) {
+    newsList.push("\n  <li>\n    <a href=\"#".concat(newsFeed[i].id, "\">\n    ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")</a>\n  </li>\n  "));
+  }
+  newsList.push('</ul>');
+  container.innerHTML = newsList.join('');
+}
+function newsDetail() {
+  var id = location.hash.slice(1);
+  var newsContent = getData(CONTENT_URL.replace('@id', id));
+  container.innerHTML = "\n    <h1>".concat(newsContent.title, "</h1>\n    <div>\n      <a href=\"#\">\uBAA9\uB85D\uC73C\uB85C</a>\n    </div>\n    ");
+}
+function router() {
+  var routePath = location.hash;
+  if (routePath === '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+window.addEventListener('hashchange', router);
+router();
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -171,7 +179,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3005" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52137" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
