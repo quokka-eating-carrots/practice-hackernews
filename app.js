1b4916038817ccd57511843b9c0df8efc70e6cc1
quokka-eating-carrots <ovzip7@gmail.com>
2022-10-16 18:20:35 +0900

    feat: Create template

--- app.js
+++ app.js
@@ -18,27 +18,34 @@ function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
 
-newsList.push('<ul>');
+  let template = `
+  <div class="container mx-auto p-4">
+    <h1>Hacker News</h1>
+    <ul>
+      {{__news_feed__}}
+    </ul>
+    <div>
+      <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+      <a href="#/page/{{__next_page__}}">다음 페이지</a>
+    </div>
+  </div>
+  `;
 
 for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i += 1) {
   newsList.push(`
   <li>
     <a href="#/show/${newsFeed[i].id}">
-    ${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+    ${newsFeed[i].title} (${newsFeed[i].comments_count})
+    </a>
   </li>
   `);
 }
 
-newsList.push('</ul>');
+template = template.replace('{{__news_feed__}}', newsList.join(''));
+template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
+template = template.replace('{{__next_page__}}', store.currentPage < 3 ? store.currentPage + 1 : 3);
 
-newsList.push(`
-  <div>
-    <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
-    <a href="#/page/${store.currentPage < 3 ? store.currentPage + 1 : 3}">다음 페이지</a>
-  </div>
-`);
-
-container.innerHTML = newsList.join('');
+container.innerHTML = template;
 }
 
 function newsDetail() {
