505b79bef7f94b3e5679c869b9255a32167e6416
quokka-eating-carrots <ovzip7@gmail.com>
2022-10-25 18:37:56 +0900

    Change file js > ts

--- app.ts
+++ app.ts
@@ -1,9 +1,25 @@
-const container = document.getElementById('root');
-const ajax = new XMLHttpRequest();
+type Store = {
+  currentPage: number;
+  feeds: NewsFeed[];
+}
+
+type NewsFeed = {
+  id: number;
+  comments_conut: number;
+  url: string;
+  user: string;
+  time_ago: string;
+  points: number;
+  title: string;
+  read?: boolean;
+}
+
+const container: HTMLElement | null = document.getElementById('root');
+const ajax: XMLHttpRequest = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
-const store = {
+const store: Store = {
   currentPage: 1,
   feeds: [],
 };
@@ -23,8 +39,16 @@ function makeFeeds(feeds) {
   return feeds;
 }
 
+function updateView(html) {
+  if (container) {
+    container.innerHTML = html;
+  } else {
+    console.error('최상위 컨테이너가 없어 UI를 진행하지 못합니다.')
+  }
+}
+
 function newsFeed() {
-  let newsFeed = store.feeds;
+  let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
 
   let template = `
@@ -82,7 +106,7 @@ template = template.replace('{{__news_feed__}}', newsList.join(''));
 template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
 template = template.replace('{{__next_page__}}', store.currentPage < 3 ? store.currentPage + 1 : 3);
 
-container.innerHTML = template;
+  updateView(template);
 }
 
 function newsDetail() {
@@ -147,7 +171,10 @@ function newsDetail() {
     return commentString.join('');
   }
 
-  container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
+  updateView(
+    template.replace('{{__comments__}}',
+    makeComment(newsContent.comments))
+  )
 }
 
 function router() {
